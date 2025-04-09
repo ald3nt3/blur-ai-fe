6cdb15c90580f5ce163416f67e54d1534c6026a2
Iwo Skoczke <iwo.skoczke@accenture.com>
2025-04-09 13:26:59 +0200

    added image preview, name and size

--- src/components/ImageUpload.tsx
+++ src/components/ImageUpload.tsx
@@ -1,14 +1,18 @@
 import "./ImageUpload.css";
-import React, { useCallback, useState } from "react";
+import { useCallback, useState } from "react";
 import { useDropzone } from "react-dropzone";
+
+const sizeMB = (size: number): number => size / 1024 ** 2;
 
 function ImageUpload() {
   const [previewUrl, setPreviewUrl] = useState<string | null>(null);
+  const [file, setFile] = useState<File | null>(null);
 
   const onDrop = useCallback((acceptedFiles: File[]) => {
     const file = acceptedFiles[0];
+    setFile(file);
     if (file && file.type.startsWith("image/")) {
-      const url = URL.createObjectURL(file)
+      const url = URL.createObjectURL(file);
       setPreviewUrl(url);
     }
   }, []);
@@ -24,17 +28,17 @@ function ImageUpload() {
   return (
     <div className="wrapper">
       {previewUrl ? (
-        <img
-          src={previewUrl}
-          alt="Preview"
-          className="preview"
-        />
+        <img src={previewUrl} alt="Preview" className="preview" />
       ) : (
-        <div className="preview placeholder" {...getRootProps()}>
-            Image preview will be displayed here
-          </div>
+        <div className="preview placeholder">
+          Image preview will be displayed here
+        </div>
       )}
-      <label htmlFor="dropzone-file" className="upload-field" {...getRootProps()}>
+      <label
+        htmlFor="dropzone-file"
+        className="upload-field"
+        {...getRootProps()}
+      >
         <div className="stack">
           <svg
             className="icon"
@@ -51,18 +55,26 @@ function ImageUpload() {
               d="M13 13h3a3 3 0 0 0 0-6h-.025A5.56 5.56 0 0 0 16 6.5 5.5 5.5 0 0 0 5.207 5.021C5.137 5.017 5.071 5 5 5a4 4 0 0 0 0 8h2.167M10 15V6m0 0L8 8m2-2 2 2"
             />
           </svg>
+          {file && (
+            <>
+              <p className="additional-text">Name: {file.name} </p>
+              <p className="additional-text">
+                Size: {sizeMB(file.size).toFixed(2)} MB
+              </p>
+            </>
+          )}
           <p className="text">
             <b>Click to upload</b> or drag and drop
           </p>
           <p className="additional-text">JPG or PNG (MAX. 800x400px)</p>
         </div>
+        <input
+          id="dropzone-file"
+          type="file"
+          accept="image/*"
+          {...getInputProps()}
+        />
       </label>
-      <input
-        id="dropzone-file"
-        type="file"
-        accept="image/*"
-        {...getInputProps()}
-      />
     </div>
   );
 }
