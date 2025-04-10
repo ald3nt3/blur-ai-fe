1df3915eec6de64c17cf94733120ff03dd0cc156
Iwo Skoczke <iwo.skoczke@accenture.com>
2025-04-10 10:40:51 +0200

    pkce code applied

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,39 @@
-import './App.css'
-import ImageUpload from './components/ImageUpload'
+import { useEffect, useState } from "react";
+import "./App.css";
+import ImageUpload from "./components/ImageUpload";
+import { Route, Routes } from "react-router-dom";
+import { login } from "./auth/auth";
+import Callback from "./pages/Callback";
 
 function App() {
+  const [loggedIn, setLoggedIn] = useState(false);
+
+  useEffect(() => {
+    const token = localStorage.getItem("id_token");
+    if (token) setLoggedIn(true);
+  }, []);
 
   return (
-    <ImageUpload/>
-  )
+    <Routes>
+      <Route path="/" element={<ImageUpload />} />
+      <Route
+        path="/auth"
+        element={
+          <div style={{ padding: "2rem" }}>
+            <h1>PKCE Auth Example</h1>
+
+            {loggedIn ? (
+              <p>You are logged in ✅</p>
+            ) : (
+              <button onClick={login}>Login</button>
+            )}
+          </div>
+        }
+      />
+
+      <Route path="/callback" element={<Callback />} />
+    </Routes>
+  );
 }
 
-export default App
+export default App;
