c46816107341f1135cf977ded953d15babd4235c
Iwo Skoczke <iwo.skoczke@accenture.com>
2025-04-11 12:48:57 +0200

    remove more unnecessary code

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,11 @@
 import "./App.css";
 import { Route, Routes } from "react-router-dom";
-import Callback from "./pages/Callback";
 import Login from "./components/Login";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Login />} />
-      <Route path="/callback" element={<Callback />} />
     </Routes>
   );
 }
--- src/config/authConfig.ts
+++ src/config/authConfig.ts
@@ -1,4 +1,4 @@
-export const clientId = ""; // Replace with your Auth0 client ID
-export const authDomain = ""; // Replace with your Auth0 domain (e.g., https://dev-xxxxx.us.auth0.com)
-export const redirectUri = ""; // Replace with your app's redirect URI
-export const audience = ""; // API audience for requesting access tokens
+export const clientId = "1n4TenO3vILIXM233CSoxCmbhRF41DCE"; // Replace with your Auth0 client ID
+export const authDomain = "https://dev-bn3hssykxatwc54p.us.auth0.com"; // Replace with your Auth0 domain (e.g., https://dev-xxxxx.us.auth0.com)
+export const redirectUri = "http://localhost:5173/"; // Replace with your app's redirect URI
+export const audience = "https://dev-bn3hssykxatwc54p.us.auth0.com/api/v2/"; // API audience for requesting access tokens
