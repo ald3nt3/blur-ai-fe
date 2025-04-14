3751ed1fe6cc251fc20b997b3aeb8e87a6ce3255
Iwo Skoczke <iwo.skoczke@accenture.com>
2025-04-14 12:25:19 +0200

    secrets moved to .env file

--- src/api/CheckBlur.ts
+++ src/api/CheckBlur.ts
@@ -1,20 +1,17 @@
 export default async function CheckBlur(file: File) {
   console.log("Uploading file...");
   const url =
-    "https://detect-image-blur-function-app.azurewebsites.net/api/test_blur?code=dlA3F4KxGNUkFUhwq2xVqu5LXmdUtc9uj2LEFQBCXPuJAzFueaY_0g==";
+    "https://detect-image-blur-function-app.azurewebsites.net/api/test_blur?code=" + import.meta.env.VITE_API_CODE;
 
   const formData = new FormData();
   formData.append("file", file, file.name);
-
-  const response = await fetch(url, {
+  
+  const data = await fetch(url, {
     method: "POST",
     body: formData,
-    mode: 'no-cors',
-  })
-    .then((res) => {
-      console.log(res);
-    })
-    .catch((err) => {
-      console.log(err);
-    });
+  }).then(async (response) => {
+    const json = await response.json();
+    console.log(json);
+    return json;
+  });
 }
--- src/config/authConfig.ts
+++ src/config/authConfig.ts
@@ -1,4 +1,4 @@
-export const clientId = "1n4TenO3vILIXM233CSoxCmbhRF41DCE"; // Replace with your Auth0 client ID
-export const authDomain = "https://dev-bn3hssykxatwc54p.us.auth0.com"; // Replace with your Auth0 domain (e.g., https://dev-xxxxx.us.auth0.com)
-export const redirectUri = "http://localhost:5173/"; // Replace with your app's redirect URI
-export const audience = "https://dev-bn3hssykxatwc54p.us.auth0.com/api/v2/"; // API audience for requesting access tokens
+export const clientId = import.meta.env.VITE_CLIENT_ID; // Replace with your Auth0 client ID
+export const authDomain = import.meta.env.VITE_AUTH_DOMAIN; // Replace with your Auth0 domain (e.g., https://dev-xxxxx.us.auth0.com)
+export const redirectUri = import.meta.env.VITE_REDIRECT_URI; // Replace with your app's redirect URI
+export const audience = import.meta.env.VITE_AUDIENCE; // API audience for requesting access tokens
